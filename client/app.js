25b3b2326767424b3b06d47abcdae5ecfab9724f
Ashish <ashish.fagna@gmail.com>
2015-12-26 22:52:16 +0530

    Showing no results when text field is blank.

--- client/app.js
+++ client/app.js
@@ -7,27 +7,26 @@ var fields = ['packageName', 'description'];
 PackageSearch = new SearchSource('packages', fields, options);
 
 Template.searchResult.helpers({
-  getPackages: function() {
-    return PackageSearch.getData({
-      transform: function(matchText, regExp) {
-        return matchText.replace(regExp, "<b>$&</b>")
-      },
-      sort: {isoScore: -1}
-    });
-  },
-  
-  isLoading: function() {
-    return PackageSearch.getStatus().loading;
-  }
+	getPackages: function() {
+		return PackageSearch.getData({
+				transform: function(matchText, regExp) {
+					return matchText.replace(regExp, "<b>$&</b>")
+				},
+				sort: {isoScore: -1}
+			}); 
+	}, 
+	isLoading: function() {
+		return PackageSearch.getStatus().loading;
+	}
 });
 
 Template.searchResult.rendered = function() {
-  PackageSearch.search('');
+	PackageSearch.search('');
 };
 
 Template.searchBox.events({
-  "keyup #search-box": _.throttle(function(e) {
-    var text = $(e.target).val().trim();
-    PackageSearch.search(text);
-  }, 200)
+	"keyup #search-box": _.throttle(function(e) {
+		var text = $(e.target).val().trim();
+		PackageSearch.search(text);
+	}, 200)
 });
--- server/app.js
+++ server/app.js
@@ -1,21 +1,24 @@
 SearchSource.defineSource('packages', function(searchText, options) {
-  var options = {sort: {isoScore: -1}, limit: 20};
-  
-  if(searchText) {
-    var regExp = buildRegExp(searchText);
-    var selector = {$or: [
-      {packageName: regExp},
-      {description: regExp}
-    ]};
+	var options = {sort: {isoScore: -1}, limit: 20};
+   
+	if(searchText.length>=1) {
+		var regExp = buildRegExp(searchText);
+		var selector = {$or: [
+			{packageName: regExp},
+			{description: regExp}
+		]};
 
-    return Packages.find(selector, options).fetch();
-  } else {
-    return Packages.find({}, options).fetch();
+	return Packages.find(selector, options).fetch();
+  } else if (searchText.length===0){ 
+		return [];// return blank array when length of text searched is zero
+  } 
+  else {
+		return Packages.find({}, options).fetch(); 
   }
 });
 
 function buildRegExp(searchText) {
-  // this is a dumb implementation
-  var parts = searchText.trim().split(/[ \-\:]+/);
-  return new RegExp("(" + parts.join('|') + ")", "ig");
+	// this is a dumb implementation
+	var parts = searchText.trim().split(/[ \-\:]+/);
+	return new RegExp("(" + parts.join('|') + ")", "ig");
 }
